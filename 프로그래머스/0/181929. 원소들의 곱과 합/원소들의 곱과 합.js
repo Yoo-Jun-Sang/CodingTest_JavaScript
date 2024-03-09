2a365fb74f6a9db2cd63705e2b45c1251d1ed1f7
준상 <83875514+Yoo-Jun-Sang@users.noreply.github.com>
2024-03-09 23:39:02 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.js
@@ -0,0 +1,16 @@
+function solution(num_list) {
+    
+     let mul = 1;
+     let sum = 0;
+    
+   for(i = 0; i < num_list.length; i++) {
+       mul *= num_list[i];
+       sum += num_list[i];
+   }
+    
+    if(mul < sum * sum) {
+           return 1;
+       } else {
+           return 0;
+       }
+}
